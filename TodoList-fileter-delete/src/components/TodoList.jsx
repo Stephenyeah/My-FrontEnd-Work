356ad56d0d527ece50021c2bedc81f19aff48539
Stephen Yang <112423397+Stephenyeah@users.noreply.github.com>
2024-02-16 16:26:29 +0200

    A delete function using the filter method

---
+++ TodoList-fileter-delete/src/components/TodoList.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+function TodoList() {
+    const [todo, setTodo] = useState({
+        description: "",
+        date: ''
+    });
+    const [todos, setTodos] = useState([]);
+
+    
+    const handleClick = () =>{
+        if(todo.description && todo.date){
+            setTodos([...todos,todo]);
+            setTodo({ description:'', date:''});
+
+        }
+
+        else{
+            alert("Type a discription first");
+        }
+        
+    }
+    const handleDelete = (index) =>{
+        setTodos(todos.filter((todo,i) => i !==index));
+        
+    }
+
+
+
+    return(
+        <div className="todo-container">
+            <br />
+            <div className="add-todo">Descritption: 
+            <input 
+                placeholder="Description"
+                value={todo.description}
+                onChange={e => setTodo({...todo, description: e.target.value})}
+            />Date:
+            <input
+                type="date"
+                value ={todo.date}
+                onChange ={e => setTodo({...todo, date: e.target.value})}
+            />
+            <button onClick={handleClick}>  Add  </button>
+            </div>
+ 
+            <table>
+            <thead>
+                <tr>
+                <th>Date</th>
+                <th>Description</th>
+                </tr>
+            </thead>
+                <tbody>
+                    {
+                        todos.map((todo,index) => 
+                            <tr key={index}>
+                           <td> {todo.date}</td>
+                            <td>{todo.description}</td>
+                            <td><button onClick={() => handleDelete(index)}>Delete</button></td>
+                           
+                        </tr>
+                         )
+                    }
+                </tbody>
+            </table>
+
+        </div>
+    );
+
+}
+
+export default TodoList;
